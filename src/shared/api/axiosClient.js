f6f75aff292aba92120112638c01f26c09d52485
uhjin1130 <rladjwls1130@naver.com>
2026-07-20 13:13:23 +0900

    feat: 찜 목록 페이지 구현

--- src/shared/api/axiosClient.js
+++ src/shared/api/axiosClient.js
@@ -4,4 +4,15 @@ const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 })
 
+axiosClient.interceptors.request.use((config) => {
+  const authData = localStorage.getItem('steamiron.auth')
+  if (authData) {
+    const { accessToken } = JSON.parse(authData)
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`
+    }
+  }
+  return config
+})
+
 export default axiosClient
